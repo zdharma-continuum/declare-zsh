11678af474aee8829fb272d452a9f9a5d0f62a1b
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-07-18 13:52:15 +0200

    .gitignore+=*.zwc etc. Imported and variables-renamed crasis as declzsh
    LICENSE+=GPLv3
    
    The project zplugin-crasis contains the zshrc-parsing technology so it
    has been decided to import the `crasis' function, which is a ZUI app,
    and to do variables and functions rename from -zcrasis* prefix into
    -declzsh* prefix, etc. Also imported @declzsh-process-buffer function,
    also with the renames. The application starts as declzsh. Now the next
    step is to tear down the ZUI-related code (upon it will be decided: Do
    we want a declzsh ZUI, semigraphical app?) and to transform the declzsh
    function into a command-line utility that parses zshrc.

---
+++ declare-zshrc.plugin.zsh
@@ -0,0 +1,8 @@
+# Handle $0 according to the Zsh Plugin Standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+
+typeset -g DECLZSH_REPO_DIR="${0:h}"
+typeset -g DECLZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/declare-zshrc"
+
+autoload @declzsh-process-buffer declzsh
