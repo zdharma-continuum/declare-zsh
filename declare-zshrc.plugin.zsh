0882fe17e2d10669dcdac1f23e6c95ada6b84a09
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-01-12 04:16:21 +0100

    plugin.zsh: Use the recent plugin-standard snippet

--- declare-zshrc.plugin.zsh
+++ declare-zshrc.plugin.zsh
@@ -1,6 +1,7 @@
 # Handle $0 according to the Zsh Plugin Standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
 
 typeset -g DECLZSH_REPO_DIR="${0:h}"
 typeset -g DECLZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/declare-zshrc"
